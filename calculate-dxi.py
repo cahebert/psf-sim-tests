9584b085299d9737cf8801c88f58b66ef332bfa5
cahebert <claireahebert@me.com>
2025-06-04 12:10:13 -0400

    dxi calc using samples

--- calculate-dxi.py
+++ calculate-dxi.py
@@ -3,22 +3,21 @@ import json
 import treecorr
 import pickle
 
-def get_dxi(corrs, keys, coefficients, namemap, trr=None, ref='desy6'):
+def get_coeff_combos(keys, coefficients, namemap, trr=None):
     """
-    Combine the given PSF residual correlations and coefficients into dxi+.
+    Combine the given PSF coefficients into dxi+/-.
     Use option parameter trr to adjust for the ratio of the tratio between two surveys.
     """
-    # fourth order
-    dxi_4 = np.zeros_like(corrs[0].xip)
-    # dxi_2 = np.zeros_like(corrs[0].xip)
+    parameters = ['g2','dg2', 'w22', 'e4', 'de4', 'w24', 'w42', 'w44']
+    coefficient_combos = {}
 
-    for v1 in ['g2','dg2', 'w22', 'e4', 'de4', 'w24', 'w42', 'w44']:
-        for v2 in ['g2','dg2', 'w22', 'e4', 'de4', 'w24', 'w42', 'w44']:
-            if v1+v2 in keys.keys() or v2+v1 in keys.keys():
-                key = v1+v2 if v1+v2 in keys.keys() else v2+v1
+    for v1 in parameters:
+        for v2 in parameters:
+            if v1+v2 in keys or v2+v1 in keys:
+                key = v1+v2 if v1+v2 in keys else v2+v1
 
-                c1 = coefficients[ref][namemap[v1]]
-                c2 = coefficients[ref][namemap[v2]]
+                c1 = coefficients[namemap[v1]]
+                c2 = coefficients[namemap[v2]]
 
                 if trr is not None:  # adjust for tratio except for alpha parameter
                     if 'alpha' not in namemap[v1]:
@@ -26,56 +25,89 @@ def get_dxi(corrs, keys, coefficients, namemap, trr=None, ref='desy6'):
                     if 'alpha' not in namemap[v2]:
                         c2 *= trr
 
-                dxi_4 += c1 * c2 * corrs[keys[key]].xip
+                coefficient_combos[key] = c1 * c2
 
-                # and if doing second order terms only:
-                # if '4' not in key:
-                #     c1 = coefficients[ref][namemap_2[v1]]
-                #     c2 = coefficients[ref][namemap_2[v2]]
+    return coefficient_combos
 
-                #     if trr is not None:  # adjust for tratio except for alpha parameter
-                #         if 'alpha' not in namemap_2[v1]:
-                #             c1 *= trr
-                #         if 'alpha' not in namemap_2[v2]:
-                #             c2 *= trr
-                #     dxi_2 += c1 * c2 * corrs[keys[key]].xip
+def run_dxi_estimate(rstats, coeffs, y10=None):
+    error_norm = 1506/150  # divide variance by the number of independent samples
 
-    return dxi_4
+    results = {'all': {}, 'no_w': {}, 'alpha': {}}
+    keys_all = {k:i for i,k in enumerate(rstats.keys())}
+    keys_ss = {k:i for i,k in enumerate(rstats.keys()) if k in ['dg2dg2','de4de4','dg2de4']}
+    keys_alpha = {k:i for i,k in enumerate(rstats.keys()) if ('e' in k or 'g' in k) and ('d' not in k)}
+    corrs = [v for k,v in rstats.items()]
+
+    for keys, tmp in zip([keys_all, keys_ss, keys_alpha], ['all', 'no_w', 'alpha']):
+        if y10:
+            def get_dxi(data, xi='xip'):
+                out = np.zeros_like(data[0].xip)
+                for k in keys.keys():
+                    if xi=='xip':
+                        out += data[keys[k]].xip * coeffs[k]
+                    elif xi=='xim':
+                        out += data[keys[k]].xim * coeffs[k]
+                return out
+
+            func_xip = lambda data: get_dxi(data,xi='xip')
+            func_xim = lambda data: get_dxi(data,xi='xim')
+
+            cov_xip = treecorr.estimate_multi_cov(corrs, 'jackknife', func=func_xip)
+            cov_xim = treecorr.estimate_multi_cov(corrs, 'jackknife', func=func_xim)
+
+            results[tmp]['xip_var'] = list(np.diag(cov_xip))
+            results[tmp]['xip'] = list(func_xip(corrs))
+            results[tmp]['xim_var'] = list(np.diag(cov_xim))
+            results[tmp]['xim'] = list(func_xim(corrs))
+            results[tmp]['r'] = list(corrs[0].meanr)
+
+        else:
+            def get_dxi(data, xi='xip'):
+                out = np.zeros_like(data[0][xi])
+                for k in keys.keys():
+                    out += np.array(data[keys[k]][xi]) * coeffs[k]
+                return out
+            dxip = get_dxi(corrs, xi='xip')
+            dxim = get_dxi(corrs, xi='xim')
+
+            results[tmp]['xip_var'] = list(np.var(dxip, axis=0) / error_norm)
+            results[tmp]['xim_var'] = list(np.var(dxim, axis=0) / error_norm)
+            results[tmp]['xip'] = list(list(d) for d in dxip)
+            results[tmp]['xim'] = list(list(d) for d in dxim)
+            results[tmp]['r'] = list(corrs[0]['meanr'])
+
+    return results
+
+results = {}
+coeff_combo = {}
 
 with open('../data/contamination-coeffs.js', 'r') as f:
-    coefficients, namemap, namemap_2, tratios = json.load(f)
+    coefficients, namemap, _, tratios = json.load(f)
 
-results = {'desy6':{'y10':{},'y1':{}}, 'hscy3':{'y10':{},'y1':{}}}
+for y10 in [False, True]:
+    # load the sim rho stats
+    if y10:
+        with open(f'../data/rho-i-full-radec-03-piff-02-cov1506.pkl', 'rb') as f:
+            rhodata = pickle.load(f)
+        tratioratio = tratios['lssty10'] / tratios['desy6']
 
-for survey in ['desy6', 'hscy3']:
-    tratioratio = tratios['lssty10'] / tratios[survey]
-    for y10 in [True, False]:
-        # load the sim rho stats
-        if y10:
-            # label = r'$\delta \xi_+^{Y10}$'
-            with open('../data/rho-i-full-radec-03-piff-02-patch25-hom1506.pkl', 'rb') as f:
-                rhodata = pickle.load(f)
-        else:
-            # label = r'$\delta \xi_+^{Y1}$'
-            with open('../data/rho-i-full-radec-03-piff-02-patch25-hom150.pkl', 'rb') as f:
-                rhodata = pickle.load(f)
+    else:
+        with open(f'../data/rho-i-full-radec-03-piff-02-50x150.json', 'r') as f:
+            rhodata = json.load(f)
+        tratioratio = tratios['lssty1'] / tratios['desy6']
 
-        corrs = [v for k,v in rhodata.items() if 'n' not in k]#list(rhodata.values())
-        keys = {k:i for i,k in enumerate(rhodata.keys()) if 'n' not in k}
+    coeffs = get_coeff_combos(rhodata.keys(), coefficients['desy6'], namemap, trr=tratioratio)
 
-        func = lambda data: get_dxi(
-            data,
-            keys,
-            coefficients,
-            namemap,
-            tratioratio,
-            ref=survey)
+    coeff_combo['y10' if y10 else 'y1'] = coeffs
+    results['y10' if y10 else 'y1'] = run_dxi_estimate(rhodata, coeffs, y10)
 
-        cov = treecorr.estimate_multi_cov(corrs, 'jackknife', func=func)
+# print(np.array(results['y10']['all']['xip']).shape)
+# print(np.array(results['y10']['all']['xip_var']).shape)
+# print(np.array(results['y1']['all']['xip']).shape)
+# print(np.array(results['y1']['all']['xip_var']).shape)
 
-        results[survey]['y10' if y10 else 'y1']['sig'] = list(np.sqrt(np.diag(cov)))
-        results[survey]['y10' if y10 else 'y1']['xip'] = list(func(corrs))
-        results[survey]['y10' if y10 else 'y1']['r'] = list(corrs[0].meanr)
-
-with open('../data/dxi-4th-jk.js', 'w') as f:
+with open('../data/dxi-i-full-radec-03-piff-02.json', 'w') as f:
     json.dump(results, f)
+
+with open('../data/coeffs-i-full-radec-03-piff-02.json', 'w') as f:
+    json.dump(coeff_combo, f)
--- dxi-plot.py
+++ dxi-plot.py
@@ -1,182 +1,109 @@
 import matplotlib.pyplot as plt
 import numpy as np
-# import pickle
+import pandas as pd
 import json
-# import treecorr
-plt.style.use('./paper.mplstyle')  # change this to a paper specific one
+from colors import color_scheme as colors
+plt.style.use('./paper.mplstyle')
 showsig = True
-
-# def get_dxi(data, coefficients, namemap_all, namemap_2, trr=None, ref='des'):
-#     """
-#     Combine the given PSF residual correlations and coefficients into dxi+.
-#     Use option parameter trr to adjust for the ratio of the tratio between two surveys.
-#     """
-#     # fourth order
-#     dxi_4 = np.zeros_like(data['g2g2'].xip)
-#     dxi_4_var = np.zeros_like(data['g2g2'].xip)
-#     dxi_2 = np.zeros_like(data['g2g2'].xip)
-#     dxi_2_var = np.zeros_like(data['g2g2'].xip)
-
-#     for v1 in ['g2','dg2', 'w22', 'e4', 'de4', 'w24', 'w42', 'w44']:
-#         for v2 in ['g2','dg2', 'w22', 'e4', 'de4', 'w24', 'w42', 'w44']:
-#             if v1+v2 in data.keys() or v2+v1 in data.keys():
-#                 key = v1+v2 if v1+v2 in data.keys() else v2+v1
-
-#                 c1 = coefficients[ref][namemap_all[v1]]
-#                 c2 = coefficients[ref][namemap_all[v2]]
-
-#                 if trr is not None:  # adjust for tratio except for alpha parameter
-#                     if 'alpha' not in namemap_all[v1]:
-#                         c1 *= trr
-#                     if 'alpha' not in namemap_all[v2]:
-#                         c2 *= trr
-
-#                 dxi_4 += c1 * c2 * data[key].xip
-#                 dxi_4_var += (c1 * c2)**2 * data[key].varxip
-
-#                 # and if doing second order terms only:
-#                 if '4' not in key:
-#                     c1 = coefficients[ref][namemap_2[v1]]
-#                     c2 = coefficients[ref][namemap_2[v2]]
-
-#                     if trr is not None:  # adjust for tratio except for alpha parameter
-#                         if 'alpha' not in namemap_2[v1]:
-#                             c1 *= trr
-#                         if 'alpha' not in namemap_2[v2]:
-#                             c2 *= trr
-#                     dxi_2 += c1 * c2 * data[key].xip
-#                     dxi_2_var += (c1 * c2)**2 * data[key].varxip
-
-#     dxi_2_var = np.sqrt(dxi_2_var)
-#     dxi_4_var = np.sqrt(dxi_4_var)
-
-#     return dxi_2, dxi_2_var, dxi_4, dxi_4_var
-
-constraint = np.loadtxt('/Users/clairealice/Documents/git/psf_req/srd_y1_tightest.txt')
-# with open('../data/contamination-coeffs.js', 'r') as f:
-#     coefficients, namemap, namemap_2, tratios = json.load(f)
-
-with open('../data/dxi-4th-jk.js', 'r') as f:
-    data = json.load(f)
 
 f, a = plt.subplots(2, 1, figsize=(3.35, 3.75), sharex=True)
 
+grey = '#dde3eb'
+
+with open(f'../data/dxi-i-full-radec-03-piff-02.json', 'r') as f:
+    data = json.load(f)
+
 for y10, ax in zip([False, True], a):
-    # load the sim rho stats
-    # if y10:
-    #     # label = r'$\delta \xi_+^{Y10}$'
-    #     with open('../data/rho-i-full-radec-03-piff-02-cov-hom1506.pkl', 'rb') as f:
-    #         rhodata = pickle.load(f)
-    # else:
-    #     # label = r'$\delta \xi_+^{Y1}$'
-    #     with open('../data/rho-i-full-radec-03-piff-02-cov-hom150.pkl', 'rb') as f:
-    #         rhodata = pickle.load(f)
-
-    for survey, color, label, marker in zip(['desy6', 'hscy3'],
-                                            ['#0038A8', '#D60270'],
-                                            # ['#EC7357', '#5775c3'],
-                                            ['DES', 'HSC'],
-                                            ['o', '^']):
-        # tratioratio = tratios['lssty10'] / tratios[survey]
-
-        # dxi_2_ref, _, dxi_4_ref, _ = get_dxi(rhodata,
-        #                                     coefficients,
-        #                                     namemap,
-        #                                     namemap_2,
-        #                                     None,
-        #                                     ref=survey)
-
-        # if y10:
-        #     print(f"Fraction of {'Y10' if y10 else 'Y1'} dxi using raw {label} coefficients:")
-        # for Trr in [0.9*tratioratio, 1.1*tratioratio, tratioratio]:
-        #     dxi_2, dxi_2_var, dxi_4, dxi_4_var = get_dxi(rhodata,
-        #                                                 coefficients,
-        #                                                 namemap,
-        #                                                 namemap_2,
-        #                                                 Trr,
-        #                                                 ref=survey)
-        #     if y10:
-        #         print(dxi_2[0]/dxi_2_ref[0], dxi_4[0]/dxi_4_ref[0])
-
-        if label == 'DES':
-            label = 'DES coefficients' #r'$\delta \xi_+^{\rm DES}$'
+    # if not y10:
+    for type, color, marker, label in zip(
+        ['all', 'no_w', 'alpha'],
+        [colors.g, colors.y, colors.p],
+        ['o','D', 's'],
+        [r'all terms', r'$\delta e^{(i)}\delta e^{(j)}$', r'$e^{(i)}x$']
+        ):
+        if y10:
+            subset = data['y10'][type]
+            xip = np.array(subset['xip'])
+            theta = np.array(subset['r'])
+            sig = np.sqrt(subset['xip_var'])
         else:
-            label = 'HSC coefficients' #r'$\delta \xi_+^{\rm HSC}$'
-
-        subset = data[survey]['y10' if y10 else 'y1']
-        theta = np.array(subset['r'])
-        xip = np.array(subset['xip'])
-
-        # second order
-        # ax.plot(theta, dxi_2, '-', color=color, lw=1.25, zorder=4, alpha=0.2)
-        # ax.plot(theta, -dxi_2, ':', color=color, lw=1.25, zorder=4, alpha=0.2)
-        # ax.errorbar(theta[dxi_2>0], dxi_2[dxi_2>0], yerr=dxi_2_var[dxi_2>0],
-        #             fmt=marker, color=color, ms=4, zorder=4, label=label, alpha=0.2)
-        # ax.errorbar(theta[dxi_2<0], -dxi_2[dxi_2<0], yerr=dxi_2_var[dxi_2<0],
-        #             fmt=marker, mfc='none', mew=0.75, color=color, ms=4, zorder=4, alpha=0.2)
+            subset = data['y1'][type]
+            xip = np.mean(subset['xip'], axis=0)
+            theta = np.array(subset['r'])
+            sig = np.sqrt(subset['xip_var'])
 
         # fourth order
-        ax.plot(theta, xip, '-', color=color, lw=1.25, zorder=4)
-        ax.plot(theta, -xip, ':', color=color, lw=1.25, zorder=4)
-        ax.errorbar(theta[xip>0], xip[xip>0], yerr=np.array(subset['sig'])[xip>0],
-                    fmt=marker, color=color, ms=4, zorder=4, label=label)
-        ax.errorbar(theta[xip<0], -xip[xip<0], yerr=np.array(subset['sig'])[xip<0],
-                    fmt=marker, mfc='none', mew=0.75, color=color, ms=4, zorder=4)
-        # label = r'$\delta \xi_+^{(4)}$'
-        # theta = rhofull['g2g2'].meanr
-        # plt.plot(theta, dxi_4, '-', color='orangered')
-        # plt.plot(theta, -dxi_4, ':', color='orangered')
-        # plt.errorbar(theta[dxi_4>0], dxi_4[dxi_4>0], yerr=dxi_4_var[dxi_4>0], fmt='o', color='orangered', label=label)
-        # plt.errorbar(theta[dxi_4<0], -dxi_4[dxi_4<0], yerr=dxi_4_var[dxi_4<0], fmt='o', mfc='none', mew=0.75, color='orangered')
+        ax.plot(theta, xip, '-', color=color, lw=1, zorder=4)
+        ax.plot(theta, -xip, ':', color=color, lw=1, zorder=4)
+        ax.errorbar(
+            theta[xip>0],
+            xip[xip>0],
+            yerr=sig[xip>0],
+            fmt=marker, color=color,
+            ms=3, zorder=4, capsize=1.5, capthick=0.5,
+            label=label
+        )
+        ax.errorbar(
+            theta[xip<0],
+            -xip[xip<0],
+            yerr=sig[xip<0],
+            fmt=marker, mfc='none', mew=0.75,
+            color=color, ms=3, zorder=4, capsize=1.5, capthick=0.5
+        )
 
     if showsig:
-        grey = '#dde3eb'
-        ## divide by sqrt(10) to get Y10, srqt(5) to adjust for sample size from redshift bins
-        ## multiply by fraction to get PSF budget from total uncertainty
-        covmin = constraint[1] #/ np.sqrt(5)
+        covfile = f'../data/thps_cov_test_matrix_{"Y10" if y10 else "Y1"}.dat'
+        constraint = pd.read_csv(covfile, delimiter='\t', header=None).to_numpy()
+        covmin = np.sqrt(np.diag(constraint)[:25])  # first half is xi+
 
-        if y10:
-            covmin /= np.sqrt(10)
-
-        for fraction, ls in zip([0.1, 0.2, 0.3], ['-', '--', ':']):
+        for fraction, ls in zip([1, 0.3],['-', '--']):
             sigma = covmin * fraction
-            ax.plot(constraint[0], sigma, ls=ls, lw=2, color=grey, zorder=2)
-            ax.fill_between(constraint[0], 0, sigma, color=grey, alpha=0.3, zorder=1)
+            ax.plot(theta, sigma, ls=ls, lw=2, color=grey, zorder=2)
+            ax.fill_between(theta, 0, sigma, color=grey, alpha=0.2, zorder=1)
 
-    ax.text(0.125, 0.875, f'Y{"10" if y10 else "1"}', fontsize=10, transform=ax.transAxes)
+a[1].text(0.1, 0.85, f'Y10', fontsize=10, transform=a[1].transAxes)
+a[0].text(0.1, 0.125, f'Y1', fontsize=10, transform=a[0].transAxes)
 
 from matplotlib.lines import Line2D
-# from matplotlib.container import ErrorbarContainer
-# from matplotlib.collections import LineCollection
-# labels = [r'DES coefficients', r'HSC coefficients']
-# lines = [Line2D([0], [0], color=c, ls='', marker=marker, label=lab)
-#             for marker, c, lab in zip(['^', 'o'], ['#D60270', '#0038A8'], labels)]
-# barline = LineCollection(np.empty((2,2,2)))
-# err = [ErrorbarContainer((line, [line], [barline]), has_yerr=True) for line in lines]
-# handles = [Line2D([0], [0], color=c, ls='', marker=marker, label=lab)
-#             for marker, c, lab in zip(['^', 'o'], ['#D60270', '#0038A8'], labels)]
-# a[0].legend(handles=err, loc='upper right', borderaxespad=0.75)
-
-sig_labels = [r'$0.1\sigma_+^{\rm min}$',
-              r'$0.2\sigma_+^{\rm min}$',
-              r'$0.3\sigma_+^{\rm min}$']
-handles = [Line2D([0], [0], color='#dde3eb', lw=2, ls=ls, label=lab)
-            for ls, lab in zip(['-', '--', ':'], sig_labels)]
-a[1].legend(handles=handles, loc='upper right', borderaxespad=0.75)
-a[0].legend(borderaxespad=0.6, handletextpad=0.1, title=r'LSST-sim $\rho$ stats and:')
+sig_labels = [r'$\sigma_{\xi_+}$', r'$0.3\sigma_{\xi_+}$']
+handles = [
+    Line2D([0], [0], color=grey, lw=2, ls=ls, label=lab)
+    for ls, lab in zip(['-', '--'], sig_labels)
+]
+a[0].legend(handles=handles, loc='upper right', borderaxespad=0.5)
+a[1].legend(loc='upper right', borderaxespad=0.5)
 
 a[1].set_xscale('log')
 a[1].set_xlabel(r'$\theta$ (arcmin)')
-a[1].set_xlim(.6, 200)
+a[1].set_xlim(.5, 200)
+
+# import pickle
+# with open('../data/xi-mcal-e460-edges-wldb-varsize-gauss.p', 'rb') as f:
+#     xi_calc = pickle.load(f)
+#     xi_calc = xi_calc[np.max(list(xi_calc.keys()))]
+
+# area_lsst = 19600  # square degrees
+# area_xi = 1.07  # square degrees
+# area_ratio = area_xi / area_lsst
+
+# # y1 data is 10x shallower, so multiply by sqrt(10)
+# adjustment_factor = area_ratio * 10
+
+# a[0].plot(
+#     xi_calc.meanr[xi_calc.meanr<2],
+#     np.sqrt(xi_calc.varxip * adjustment_factor)[xi_calc.meanr<2],
+#     ls='--',
+#     color=colors.p,
+#     lw=2,
+#     zorder=2,
+# )
 
 for ax in a:
     ax.set_yscale('log', nonpositive='clip')
     ax.set_ylabel(r'$\delta\xi_+^{\rm PSF}(\theta)$')
-    ax.set_ylim(bottom=5e-10, top=2e-5)
-    # ax.tick_params(axis='both', which='both', direction='out', top=0, right=0)
+    ax.set_ylim(bottom=4e-10, top=1e-5)
 
-# plt.subplots_adjust(hspace=0.01, top=0.92, right=0.925, bottom=0.85, left=0.08)
 plt.subplots_adjust(hspace=0)
 
-plt.savefig('../figures/dxi-y1y10-i-full-radec-03-piff-02-jk.jpg', dpi=300)
-# plt.show()
+plt.savefig('../figures/dxi-i-full-radec-03-piff-02-2.jpg', dpi=300)
+plt.show()
